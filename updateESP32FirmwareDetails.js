fbe9427ace85daf5d522cbc4de7bce6f924b5dca
Paul Kerspe <Paul@kerspe.com>
2023-07-07 10:42:47 +0200

    add dynamic path to binary with tag version

--- updateESP32FirmwareDetails.js
+++ updateESP32FirmwareDetails.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 
-function replaceVersionNumberAndFiles(filename, newVersion) {
+function replaceVersionNumberAndFiles(filename, newVersion, tagName) {
     try {
         const fileContents = fs.readFileSync(filename, 'utf8');
         const jsonData = JSON.parse(fileContents);
 
         jsonData['greenhouseESPFirmwareVersion'] = newVersion;
+        jsonData['greenhouseESPFirmwareVersion'] = 'https://raw.githubusercontent.com/IoT-Smart-Greenhouse/IoT-Greenhouse-OTA/' + tagName + '/esp32/firmware.bin';
 
         const updatedContents = JSON.stringify(jsonData, null, 2);
         fs.writeFileSync(filename, updatedContents);
@@ -16,7 +17,7 @@ function replaceVersionNumberAndFiles(filename, newVersion) {
     }
 }
 
-function readVersionNumberFromPackageJson(packageJsonPath){
+function readVersionNumberFromPackageJson(packageJsonPath) {
     const fileContents = fs.readFileSync(packageJsonPath, 'utf8');
     const jsonData = JSON.parse(fileContents);
     return jsonData.version;
@@ -24,10 +25,13 @@ function readVersionNumberFromPackageJson(packageJsonPath){
 
 const jsonVersionsFile = 'stable_versions.json';
 const packageJsonPath = process.env.PACKAGE_JSON_PATH;
-console.log(packageJsonPath);
-if(packageJsonPath){
-    const newVersion = readVersionNumberFromPackageJson(packageJsonPath);
-    replaceVersionNumberAndFiles(jsonVersionsFile, newVersion);
-} else {
+const tagName = process.env.GIT_TAG_NAME;
+
+if (!tagName)
+    throw new Error("Missing environment variable GIT_TAG_NAME to provide tag name for download file path");
+
+if (!packageJsonPath)
     throw new Error("Missing environment variable PACKAGE_JSON_PATH to provide path to package.json file to read version number from");
-}
+
+const newVersion = readVersionNumberFromPackageJson(packageJsonPath);
+replaceVersionNumberAndFiles(jsonVersionsFile, newVersion, tagName);
